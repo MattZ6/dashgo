2404eaa002e439c35df82bc9b22fe8e54ecae570
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-04-04 12:02:50 -0300

    feat: add login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,65 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <h1>DashGO</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        flexDir="column"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="purple.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="email">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="purple.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="purple"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
